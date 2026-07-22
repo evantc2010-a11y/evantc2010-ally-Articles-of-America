dee5e88b66ffcb9343c2272f4c1f8140ca6594bc
evantc2010-a11y <evantc2010@gmail.com>
2026-07-22 15:55:12 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,35 +1,28 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
-  // Copy static assets
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("images");
 
-  // Date filter
   eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("LLLL dd, yyyy");
   });
 
-  // Get articles by category
   eleventyConfig.addFilter("byCategory", (articles, category) => {
     return articles.filter(a => a.data.category === category);
   });
 
-  // Limit filter
   eleventyConfig.addFilter("limit", (array, n) => array.slice(0, n));
 
   return {
+    pathPrefix: "/evantc2010-ally-Articles-of-America/",
     dir: {
       input: ".",
       output: "_site",
       includes: "_includes",
       data: "_data"
     },
-    pathPrefix: "/evantc2010-ally-Articles-of-America/",
     templateFormats: ["njk", "md", "html"],
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk"
-  };
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk"
   };
