216e663878407674241c51d02b398f16727dc4c9
evantc2010-a11y <evantc2010@gmail.com>
2026-07-23 18:12:42 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,10 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter("limit", (array, n) => array.slice(0, n));
 
+  eleventyConfig.addCollection("articles", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("articles/*.md");
+  });
+
   return {
     dir: {
       input: ".",
